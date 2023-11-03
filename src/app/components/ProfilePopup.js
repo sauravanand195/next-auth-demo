365ff4e5f46de4a079670cc3014eebdc0d61b2a1
saurav anand <saurav.anand1@justdial.com>
2023-11-03 16:31:20 +0530

    manage profile updated

--- src/app/components/ProfilePopup.js
+++ src/app/components/ProfilePopup.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Avatar, Card, CardContent, CardHeader, CardMedia, Checkbox, CssBaseline, Dialog, FormControlLabel, Grid, IconButton, TextField, Typography } from "@mui/material"
+import { Avatar, Card, Dialog, FormControl, FormHelperText, InputAdornment, TextField, Typography } from "@mui/material"
 import EditIcon from '@mui/icons-material/Edit';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 const ProfilePopup = ({ open, handleClose, stringAvatar, session }) => {
     return (
@@ -17,12 +18,12 @@ const ProfilePopup = ({ open, handleClose, stringAvatar, session }) => {
                 </Typography>
                 <div style={{ display: 'flex', padding: '20px 0 20px 0' }}>
                     <Avatar sx={{ width: 45, height: 45 }} {...stringAvatar(session?.user?.username)} />
-                    <Typography sx={{ color: 'grey', fontWeight: 'bold', padding: "0px 0px 0px 20px", alignItems: 'middle' }}>{session?.user?.username}<EditIcon color='primary' sx={{ paddingLeft: '8px', paddingTop: '8px' }} /></Typography>
+                    <Typography sx={{ color: 'grey', fontWeight: 'bold', padding: "0px 0px 0px 20px", alignItems: 'middle' }}>{session?.user?.username}<EditIcon color='primary' sx={{ paddingLeft: '8px', paddingTop: '8px', cursor: 'pointer' }} /></Typography>
                 </div>
                 <Typography sx={{ fontWeight: 'bold', borderBottom: '1px solid lightgrey' }} >
                     Email Address
                 </Typography>
-                <Typography sx={{ color: 'grey', fontWeight: 'bold', alignItems: 'middle' }}>{'saurav@anand.test'}<EditIcon color='primary' sx={{ paddingLeft: '8px', paddingTop: '8px' }} /></Typography>
+                <Typography sx={{ color: 'grey', fontWeight: 'bold', alignItems: 'middle' }}>{'saurav@anand.test'}<EditIcon color='primary' sx={{ paddingLeft: '8px', paddingTop: '8px', cursor: 'pointer' }} /></Typography>
                 <Typography sx={{ fontWeight: 'bold', borderBottom: '1px solid lightgrey', paddingTop: '20px' }} >
                     Phone Numbers
                 </Typography>
@@ -34,6 +35,22 @@ const ProfilePopup = ({ open, handleClose, stringAvatar, session }) => {
                 </Typography>
                 <Typography sx={{ fontWeight: 'bold', borderBottom: '1px solid lightgrey', paddingTop: '20px' }} >
                     Password
+                </Typography>
+                <Typography sx={{ color: 'black', fontWeight: 'bolder', alignItems: 'middle', lineHeight: 1 }}>
+                    <FormControl sx={{ m: 1, width: '17ch' }} variant="standard">
+                        <TextField type='password' value={'saurav@1995'} variant="standard" margin="normal"
+                            InputProps={{
+                                endAdornment:
+                                    <InputAdornment position="end">
+                                        {true ? <VisibilityOff color='primary' sx={{ cursor: 'pointer' }} /> : <Visibility color='primary' sx={{ cursor: 'pointer' }} />}
+                                    </InputAdornment>,
+                                disableUnderline: true,
+                            }}
+                        />
+                        <FormHelperText sx={{ color: '#1976d2', marginTop: "-3px", cursor: 'pointer' }}>
+                            Change your password
+                        </FormHelperText>
+                    </FormControl>
                 </Typography>
             </Card>
         </Dialog>
