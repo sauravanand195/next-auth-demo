df8b07835ebf42e65a93935f5f16fb01e01054d9
saurav anand <saurav.anand1@justdial.com>
2023-11-07 12:44:43 +0530

    Changes updated

--- src/app/protected/products/page.js
+++ src/app/protected/products/page.js
@@ -1,13 +1,31 @@
+"use client"
+import { Button, CssBaseline, Grid, Typography } from '@mui/material'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const ProductsPage = () => {
+    const router = useRouter()
     return (
-        <div>
-            This is Products Page
-            <div>
-                Create route to Todo App
-            </div>
-        </div>
+        <>
+            <CssBaseline />
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 2, sm: 3, md: 4 }} sx={{ padding: '20px' }}>
+                <Grid item sm={9}>
+                    <Typography component="h2" variant="h6" align="left" color="text.primary">
+                        Organize your work and life, finally.
+                    </Typography>
+                    <Typography variant="h7" align="left" color="text.secondary" paragraph>
+                        Maintain your day-to-day tasks or list everything that you have to do, with the most important tasks at the top of the list, and the least important tasks at the bottom.
+                    </Typography>
+                </Grid>
+                <Grid item sm={3}>
+                    <div style={{ position: 'relative', top: '45%', left: '50%', transform: "translate(-50%, -50%)" }}>
+                        <Button variant="contained" onClick={() => { router.push('/todo') }}>
+                            Open Todo App
+                        </Button>
+                    </div>
+                </Grid>
+            </Grid>
+        </>
     )
 }
 
--- src/app/todo/page.js
+++ src/app/todo/page.js
@@ -33,10 +33,10 @@ const Index = () => {
             <main style={{ minHeight: '78vh' }}>
                 <Box sx={{ pt: 4 }}>
                     <Container maxWidth="md">
-                        <Typography component="h2" variant="h4" align="center" color="text.primary" gutterBottom>
+                        <Typography component="h2" variant="h5" align="center" color="text.primary" gutterBottom>
                             Organize your work and life, finally.
                         </Typography>
-                        <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                        <Typography variant="h6" align="center" color="text.secondary" paragraph>
                             Maintain your day-to-day tasks or list everything that you have to do, with the most important tasks at the top of the list, and the least important tasks at the bottom.
                         </Typography>
                     </Container>
