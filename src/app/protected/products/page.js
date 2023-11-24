ca79e408ee296439816d2cf7f36cfd9968c1cc32
saurav anand <saurav.anand1@justdial.com>
2023-11-24 15:54:08 +0530

    product updated

--- src/app/protected/products/page.js
+++ src/app/protected/products/page.js
@@ -20,7 +20,23 @@ const ProductsPage = () => {
                 <Grid item sm={3}>
                     <div style={{ position: 'relative', top: '45%', left: '50%', transform: "translate(-50%, -50%)" }}>
                         <Button variant="contained" onClick={() => { router.push('/todo') }}>
-                            Open Todo App
+                            Start Organising
+                        </Button>
+                    </div>
+                </Grid>
+            </Grid>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 2, sm: 3, md: 4 }} sx={{ padding: '20px' }}>
+                <Grid item sm={9}>
+                    <Typography component="h2" variant="h6" align="left" color="text.primary">
+                        Shop online with great deals over a wide range of products.
+                    </Typography>
+                    <Typography variant="h7" align="left" color="text.secondary" paragraph>
+                        Enjoy unlimited Free Shipping, early access to Lightning Deals and more. Explore deals on top categories like Electronics, Fashion & more online today! Best Deals. Low Prices. No Cost EMI Available.                    </Typography>
+                </Grid>
+                <Grid item sm={3}>
+                    <div style={{ position: 'relative', top: '45%', left: '50%', transform: "translate(-50%, -50%)" }}>
+                        <Button variant="contained" onClick={() => { router.push('/todo') }}>
+                            Start Shopping
                         </Button>
                     </div>
                 </Grid>
